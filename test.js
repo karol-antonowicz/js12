fedbc2c355ece9c56928767a4871e637d9bfcad3
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:24:07 +0200

    Initial commit - do you like meat?

---
+++ test.js
@@ -0,0 +1,22 @@
+var youLikeMeat = true;
+
+
+if (youLikeMeat){
+    document.write("Here is the meaty menu...");
+}
+
+/* if true napisze here is..; jak false to nic sie nie stanie */
+
+var myNum = 10;
+
+if (myNum > 10){
+    document.write("ok!") // tutaj nic nie napisze
+} else {
+    document.write("chuj z tym") // w tym przypadku napisze chu
+}
+
+if (myNum == 10){
+    document.write("ok!") // tutaj napisze
+} else {
+    document.write("chuj z tym") // w tym przypadku nie napisze
+}
